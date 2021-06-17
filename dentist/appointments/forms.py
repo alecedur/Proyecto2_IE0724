71d2ec1a7656722e6d79181c5a277172ae863a03
jose9706 <joseal9706@gmail.com>
2021-06-16 22:55:21 -0600

    delete comment

--- dentist/appointments/forms.py
+++ dentist/appointments/forms.py
@@ -52,19 +52,4 @@ class superuserForm(forms.ModelForm):
             'password' : 'Password',            
         }
     key = forms.CharField(required=True, max_length=20)    
-# class PetForm(forms.ModelForm):
 
-#     class Meta:
-#         model = Pet
-#         fields = [
-#             'name',
-#             'gender',
-#             'age',
-#             'species',
-#         ]
-#         labels = {
-#             'name': 'Name',
-#             'gender': 'Gender',
-#             'age': 'Age',
-#             'species': 'Species',
-#         }
